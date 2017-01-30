642f905c94200557a22aaaf854db1fd73641e66f
TiredBaboon <toc362@gmail.com>
2017-01-30 12:17:05 -0600

    Fixed bug in output of results.

--- src/main/java/com/website/auto/Exec.java
+++ src/main/java/com/website/auto/Exec.java
@@ -2,6 +2,7 @@ package com.website.auto;
 
 import com.jcraft.jsch.*;
 import java.io.*;
+import java.lang.StringBuilder;
 
 public class Exec {
     public static void main(String[] args) throws IOException, JSchException {
@@ -11,49 +12,44 @@ public class Exec {
         String cmd = args[3];
 
         JSch jsch = new JSch();
-
         Session session = jsch.getSession(user, host, 22);
 
-        session.setConfig("StrictHostKeyChecking", "no");
-        session.setPassword(pswd);
-        session.connect();
+        try {
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.setPassword(pswd);
+            session.connect();
+        } catch (JSchException e) {
+            System.out.println(e);
+        }
 
         Channel channel = session.openChannel("exec");
-        ((ChannelExec)channel).setCommand(cmd);
 
-        channel.setInputStream(null);
-        ((ChannelExec)channel).setErrStream(System.err);
-        InputStream in = channel.getInputStream();
-        channel.connect();
+        try {
+            ((ChannelExec)channel).setCommand(cmd);
 
-        byte[] tmp = new byte[1024];
-
-        System.out.println("Session: " + session.isConnected());
-        System.out.println("Channel: " + channel.isConnected());
-
-        while (true) {
-            while (in.available() > 0) {
-                int i = in.read(tmp, 0, 1024);
-
-                if (i < 0) {
-                    break;
-                }
-
-                //Something is happening where in.available() never goes above 0.  Troubleshooting required.
-                //Try verifying connectivity to the host first with something like session.isConnected() and
-                //channel.isConnected()
-
-                System.out.println(new String(tmp, 0, i));
-            }
-
-            if (channel.isClosed()) {
-                if (channel.getExitStatus() == 0) {
-                    System.out.println("Command executed successfully.");
-                }
-
-                break;
-            }
+            channel.setInputStream(null);
+            ((ChannelExec)channel).setErrStream(System.err);
+            channel.connect();
+        } catch (JSchException e) {
+            System.out.println(e);
         }
+
+        InputStream stdout = channel.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stdout));
+
+        StringBuilder results = new StringBuilder();
+        int int_result;
+
+        try {
+            while ((int_result = bufferedReader.read()) != -1) {
+                results.append((char)int_result);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        System.out.println(results.toString());
+        System.out.printf("Exit status: %d%n", channel.getExitStatus());
 
         channel.disconnect();
         session.disconnect();
