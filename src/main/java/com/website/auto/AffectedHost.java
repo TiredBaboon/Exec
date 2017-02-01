65ce4d6f7f94e9cc11b4a4e6ef0eb0534bc6034a
TiredBaboon <toc362@gmail.com>
2017-01-31 21:32:07 -0600

    Compactualized try-catch blocks.

--- src/main/java/com/website/auto/AffectedHost.java
+++ src/main/java/com/website/auto/AffectedHost.java
@@ -15,67 +15,58 @@ public class AffectedHost {
         this.pswd = pswd;
     }
 
+    private static void cleanUp(Session session, Channel channel) {
+        channel.disconnect();
+        session.disconnect();
+    }
+
     public int exec(String cmd) {
         JSch jsch = new JSch();
         Session session = null;
+        Channel channel = null;
+        InputStream stdout = null;
+        BufferedReader bufferedReader = null;
+        StringBuilder results = new StringBuilder();
+        int int_result = 1;
 
         try {
             session = jsch.getSession(this.user, this.host, 22);
-        } catch (JSchException e) {
-            System.out.println(e);
-        }
-
-        try {
             session.setConfig("StrictHostKeyChecking", "no");
             session.setPassword(this.pswd);
             session.connect();
-        } catch (JSchException e) {
-            System.out.println(e);
-        }
 
-        Channel channel = null;
-
-        try {
             channel = session.openChannel("exec");
-        } catch (JSchException e) {
-            System.out.println(e);
-        }
-
-        try {
             ((ChannelExec)channel).setCommand(cmd);
 
             channel.setInputStream(null);
             ((ChannelExec)channel).setErrStream(System.err);
+
             channel.connect();
         } catch (JSchException e) {
-            System.out.println(e);
+            cleanUp(session, channel);
+            System.out.println("Exception: " + e);
+
+            return 1;
         }
 
-        InputStream stdout = null;
 
         try {
             stdout = channel.getInputStream();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
+            bufferedReader = new BufferedReader(new InputStreamReader(stdout));
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stdout));
-
-        StringBuilder results = new StringBuilder();
-        int int_result;
-
-        try {
             while ((int_result = bufferedReader.read()) != -1) {
                 results.append((char)int_result);
             }
         } catch (IOException e) {
-            System.out.println(e);
+            cleanUp(session, channel);
+            System.out.println("Exception: " + e);
+
+            return 1;
         }
 
-        System.out.println(results.toString());
+        cleanUp(session, channel);
 
-        channel.disconnect();
-        session.disconnect();
+        System.out.println(results.toString());
 
         return channel.getExitStatus();
     }
