39b56ceb2f624063788dfd033a394dd0c61729e8
TiredBaboon <toc362@gmail.com>
2017-04-12 02:31:36 -0500

    Fixed constructor args in Remote.java

--- src/main/java/com/website/auto/Remote.java
+++ src/main/java/com/website/auto/Remote.java
@@ -3,11 +3,9 @@ package com.website.auto;
 public class Remote {
     public static void main(String[] args) {
         String host = args[0]; 
-        String user = args[1];
-        String pswd = args[2];
-        String cmd = args[3];
+        String cmd = args[1];
 
-        AffectedHost remoteHost = new AffectedHost(host, user, pswd);
+        AffectedHost remoteHost = new AffectedHost(host);
 
         int ret_code = remoteHost.exec(cmd);
 
