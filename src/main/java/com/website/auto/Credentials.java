722fb2c05cd50dfac99b9d355278249621cbf780
TiredBaboon <toc362@gmail.com>
2017-04-12 03:29:37 -0500

    Added credentials property file.

--- src/main/java/com/website/auto/Credentials.java
+++ src/main/java/com/website/auto/Credentials.java
@@ -1,20 +1,39 @@
 package com.website.auto;
 
+import java.util.Properties;
+import java.io.*;
+
 public class Credentials {
     private String user;
     private String pswd;
 
+    private static String credentialsFile = "/home/toc362_ubuntu/scripts/java/Exec/src/main/resources/credentials/credentials.properties";
+    private Properties credentials = new Properties();
+
     public Credentials(String host) {
-        this.user = findUser(host);
-        this.pswd = findPswd(host);
+        try {
+            FileInputStream in = new FileInputStream(credentialsFile);
+
+            try {
+                credentials.load(in);
+                in.close();
+
+                this.user = findUser(host);
+                this.pswd = findPswd(host);
+            } catch (IOException e) {
+                System.out.println("Error: IOException");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found");
+        }
     }
 
     private String findUser(String host) {
-        return "tyler";
+        return this.credentials.getProperty("user");
     }
 
     private String findPswd(String host) {
-        return "chbs78969";
+        return this.credentials.getProperty("pswd");
     }
 
     public String getUser() {
